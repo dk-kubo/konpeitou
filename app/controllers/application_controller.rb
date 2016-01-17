c0e3d6edd9b16b6eab115452fe4a5406e329d388
kubo daiki <pipion.214@gmail.com>
2016-01-17 19:31:08 +0900

    deviseのログイン画面変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,29 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
+
+	protected
+		def configure_permitted_parameters
+	      [:name].each do |param|
+	        devise_parameter_sanitizer.for(:sign_up) << param
+	    end
+
+	      [:name].each do |param|
+	        devise_parameter_sanitizer.for(:account_update) << param
+		end
+	end
+
+
+	def after_sign_in_path_for(resource)
+		index_top_path
+	end
+	def after_sign_out_path_for(resource)
+		new_user_session_path
+	end
+
 end
